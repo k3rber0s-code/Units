fda5fba1d760b0f9390145423aff4b9a213cff97
k3rber0s-code <tomanoem@natur.cuni.cz>
2022-04-19 11:18:38 +0200

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -70,7 +70,6 @@ struct static_vector_add<static_vector<0>, static_vector<0>>
 	using type = static_vector<0>;
 };
 
-
 template <typename, size_t Capacity, int Index>
 struct static_vector_create
 {
@@ -79,19 +78,19 @@ struct static_vector_create
 template <int Index, size_t Capacity>
 struct static_vector_create<static_vector<>, Capacity, Index>
 {
-	using type = typename static_vector_create<push_back<static_vector<>, 0>::type, Capacity-1, Index-1>::type;
+	using type = typename static_vector_create<push_back<static_vector<>, 0>::type, Capacity - 1, Index - 1>::type;
 };
 
 template <int Index, size_t Capacity, int Head, int... Tail>
 struct static_vector_create<static_vector<Head, Tail...>, Capacity, Index>
 {
-	using type = typename static_vector_create<typename push_back<static_vector<Head, Tail...>, 0>::type, Capacity-1, Index-1>::type;
+	using type = typename static_vector_create<typename push_back<static_vector<Head, Tail...>, 0>::type, Capacity - 1, Index - 1>::type;
 };
 
 template <size_t Capacity, int Head, int... Tail>
 struct static_vector_create<static_vector<Head, Tail...>, Capacity, 0>
 {
-	using type = typename static_vector_create<typename push_back<static_vector<Head, Tail...>, 1>::type, Capacity-1, -1>::type;
+	using type = typename static_vector_create<typename push_back<static_vector<Head, Tail...>, 1>::type, Capacity - 1, -1>::type;
 };
 
 template <int Index, int Head, int... Tail>
@@ -103,26 +102,38 @@ template <typename TEnum, typename TPowers>
 struct unit;
 
 template <typename TEnum, TEnum Index>
-using basic_unit = typename unit<TEnum, typename static_vector_create<TEnum::_count, Index>::type>;
+using basic_unit = unit<TEnum, typename static_vector_create<static_vector<>, (int)TEnum::_count, (int)Index>::type>;
 
+enum class si_units
+{
+	metre,
+	kilogram,
+	ampere,
+	kelvin,
+	second,
+	mole,
+	candela,
+	_count
+};
 
 int main()
 {
+	using second = basic_unit<si_units, si_units::second>;
 	using v = static_vector<4, 3, 2, 1, 0>;
 	using v2 = static_vector<4, 4, 2, 1, 0>;
 	using v3 = push_back<v2, 9>::type;
 	using v4 = static_vector<at<v, 0>::value + at<v, 0>::value>;
-	//std::cout << at<v, 0>::value << std::endl;
+	// std::cout << at<v, 0>::value << std::endl;
 	using v7 = typename static_vector_create<static_vector<>, 3, 1>::type;
 	std::cout << at<v7, 0>::value << " ";
 	std::cout << at<v7, 1>::value << " ";
 	std::cout << at<v7, 2>::value << " ";
 
+	std::cout << second << std::endl;
 
-	
 	using v5 = typename static_vector_add<v, v2>::type;
 	using v6 = static_vector<0>;
-	//std::cout << at<v6, 0>::value << "v6" << std::endl;
+	// std::cout << at<v6, 0>::value << "v6" << std::endl;
 
 	// std::cout << at<v5, 0>::value << std::endl;
 	// std::cout << at<v5, 1>::value << std::endl;
