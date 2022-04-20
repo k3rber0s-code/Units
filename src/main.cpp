d7d628fa5ebd230a9dff353c227c993d9b96a348
k3rber0s-code <tomanoem@natur.cuni.cz>
2022-04-20 17:00:08 +0200

    add multiple added

--- src/main.cpp
+++ src/main.cpp
@@ -108,7 +108,7 @@ struct static_vector_create<static_vector<>, Capacity, Index>
 template <size_t Capacity>
 struct static_vector_create<static_vector<>, Capacity, 0>
 {
-	using type = typename static_vector_create<push_back<static_vector<>, 1>::type, Capacity - 1, - 1>::type;
+	using type = typename static_vector_create<push_back<static_vector<>, 1>::type, Capacity - 1, -1>::type;
 };
 
 template <int Index, size_t Capacity, int Head, int... Tail>
@@ -140,8 +140,53 @@ template <typename TEnum, TEnum Index>
 using basic_unit = unit<TEnum, typename static_vector_create<static_vector<>, (int)TEnum::_count, (int)Index>::type>;
 
 template <typename TDividendUnit, typename TDivisorUnit>
+// requires (std::is_same<typename TDividendUnit::type, typename TDivisorUnit::type>::value)
 using divided_unit = unit<typename TDividendUnit::type,
-typename static_vector_subtract<typename TDividendUnit::vector, typename TDivisorUnit::vector>::type>;
+						  typename static_vector_subtract<typename TDividendUnit::vector, typename TDivisorUnit::vector>::type>;
+
+template <typename U1, typename... Us>
+struct are_same : std::conjunction<std::is_same<U1, Us>...>
+{
+};
+
+template <typename... Vector>
+struct static_vector_add_multiple;
+
+template <int... E1, int... E2, typename... Vector>
+struct static_vector_add_multiple<static_vector<E1...>, static_vector<E2...>, Vector...>
+{
+	using type = static_vector_add_multiple<typename static_vector_add<static_vector<E1...>, static_vector<E2...>>::type, Vector...>::type;
+};
+
+template <int... E1, int... E2>
+struct static_vector_add_multiple<static_vector<E1...>, static_vector<E2...>>
+{
+	using type = typename static_vector_add<static_vector<E1...>, static_vector<E2...>>::type;
+};
+
+template <int E1H, int... E1T, int E2H, int... E2T>
+struct static_vector_add<static_vector<E1H, E1T...>, static_vector<E2H, E2T...>>
+{
+	using type = typename static_vector_cat<static_vector<E1H + E2H>, typename static_vector_add<static_vector<E1T...>, static_vector<E2T...>>::type>::type;
+};
+
+template <int... E2>
+struct static_vector_add<static_vector<0>, static_vector<E2...>>
+{
+	using type = static_vector<E2...>;
+};
+
+template <int... E1>
+struct static_vector_add<static_vector<E1...>, static_vector<0>>
+{
+	using type = static_vector<E1...>;
+};
+
+template <>
+struct static_vector_add<static_vector<0>, static_vector<0>>
+{
+	using type = static_vector<0>;
+};
 
 enum class si_units
 {
@@ -164,7 +209,7 @@ int main()
 	using v2 = static_vector<4, 4, 2, 1, 0>;
 	using v3 = typename static_vector_subtract<v, v2>::type;
 	using v4 = divided_unit<metre, second>;
-	
+
 	std::cout << at<v3, 0>::value << std::endl;
 	std::cout << at<v3, 1>::value << std::endl;
 	std::cout << at<v3, 2>::value << std::endl;
