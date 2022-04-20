1d2cde524fdce6fc47afec3f8d9a2ff3750ee9a1
k3rber0s-code <tomanoem@natur.cuni.cz>
2022-04-20 17:07:20 +0200

    made it nicer

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
+/////////////////////////// STATIC VECTOR ///////////////////////////
 template <int... Values>
 struct static_vector
 {
-	size_t s = sizeof...(Values);
+	static constexpr size_t s = sizeof...(Values);
 };
 
+/////////////////////////// PUSH BACK ///////////////////////////
 template <typename Vector, int Added>
 struct push_back
 {
@@ -17,6 +19,7 @@ struct push_back<static_vector<Values...>, Added>
 	using type = static_vector<Values..., Added>;
 };
 
+/////////////////////////// AT ///////////////////////////
 template <typename Vector, size_t Index>
 struct at
 {
@@ -34,6 +37,7 @@ struct at<static_vector<Head, Tail...>, Index>
 	static constexpr auto value = at<static_vector<Tail...>, Index - 1>::value;
 };
 
+/////////////////////////// STATIC CAT ///////////////////////////
 template <typename, typename>
 struct static_vector_cat;
 
@@ -42,6 +46,8 @@ struct static_vector_cat<static_vector<E1...>, static_vector<E2...>>
 {
 	using type = static_vector<E1..., E2...>;
 };
+
+/////////////////////////// STATIC ADD ///////////////////////////
 
 template <typename, typename>
 struct static_vector_add;
@@ -70,6 +76,47 @@ struct static_vector_add<static_vector<0>, static_vector<0>>
 	using type = static_vector<0>;
 };
 
+template <int E1H, int... E1T, int E2H, int... E2T>
+struct static_vector_add<static_vector<E1H, E1T...>, static_vector<E2H, E2T...>>
+{
+	using type = typename static_vector_cat<static_vector<E1H + E2H>, typename static_vector_add<static_vector<E1T...>, static_vector<E2T...>>::type>::type;
+};
+
+template <int... E2>
+struct static_vector_add<static_vector<0>, static_vector<E2...>>
+{
+	using type = static_vector<E2...>;
+};
+
+template <int... E1>
+struct static_vector_add<static_vector<E1...>, static_vector<0>>
+{
+	using type = static_vector<E1...>;
+};
+
+template <>
+struct static_vector_add<static_vector<0>, static_vector<0>>
+{
+	using type = static_vector<0>;
+};
+/////////////////////////// STATIC ADD MULTIPLE ///////////////////////////
+
+template <typename... Vector>
+struct static_vector_add_multiple;
+
+template <int... E1, int... E2, typename... Vector>
+struct static_vector_add_multiple<static_vector<E1...>, static_vector<E2...>, Vector...>
+{
+	using type = static_vector_add_multiple<typename static_vector_add<static_vector<E1...>, static_vector<E2...>>::type, Vector...>::type;
+};
+
+template <int... E1, int... E2>
+struct static_vector_add_multiple<static_vector<E1...>, static_vector<E2...>>
+{
+	using type = typename static_vector_add<static_vector<E1...>, static_vector<E2...>>::type;
+};
+
+/////////////////////////// STATIC NEGATE ///////////////////////////
 template <typename>
 struct static_vector_negate;
 
@@ -85,6 +132,7 @@ struct static_vector_negate<static_vector<0>>
 	using type = static_vector<0>;
 };
 
+/////////////////////////// STATIC VECTOR SUBTRACT ///////////////////////////
 template <typename, typename>
 struct static_vector_subtract;
 
@@ -93,6 +141,8 @@ struct static_vector_subtract<static_vector<E1H, E1T...>, static_vector<E2H, E2T
 {
 	using type = typename static_vector_add<static_vector<E1H, E1T...>, typename static_vector_negate<static_vector<E2H, E2T...>>::type>::type;
 };
+
+/////////////////////////// STATIC VECTOR CREATE ///////////////////////////
 
 template <typename, size_t Capacity, int Index>
 struct static_vector_create
@@ -129,6 +179,7 @@ struct static_vector_create<static_vector<Head, Tail...>, 0, Index>
 	using type = static_vector<Head, Tail...>;
 };
 
+/////////////////////////// UNIT ///////////////////////////
 template <typename TEnum, typename TPowers>
 struct unit
 {
@@ -136,57 +187,27 @@ struct unit
 	using vector = TPowers;
 };
 
+/////////////////////////// BASIC UNIT ///////////////////////////
+
 template <typename TEnum, TEnum Index>
 using basic_unit = unit<TEnum, typename static_vector_create<static_vector<>, (int)TEnum::_count, (int)Index>::type>;
+
+/////////////////////////// DIVIDED UNIT ///////////////////////////
 
 template <typename TDividendUnit, typename TDivisorUnit>
 // requires (std::is_same<typename TDividendUnit::type, typename TDivisorUnit::type>::value)
 using divided_unit = unit<typename TDividendUnit::type,
 						  typename static_vector_subtract<typename TDividendUnit::vector, typename TDivisorUnit::vector>::type>;
 
+
+/////////////////////////// ARE SAME ///////////////////////////
+
 template <typename U1, typename... Us>
 struct are_same : std::conjunction<std::is_same<U1, Us>...>
 {
 };
 
-template <typename... Vector>
-struct static_vector_add_multiple;
 
-template <int... E1, int... E2, typename... Vector>
-struct static_vector_add_multiple<static_vector<E1...>, static_vector<E2...>, Vector...>
-{
-	using type = static_vector_add_multiple<typename static_vector_add<static_vector<E1...>, static_vector<E2...>>::type, Vector...>::type;
-};
-
-template <int... E1, int... E2>
-struct static_vector_add_multiple<static_vector<E1...>, static_vector<E2...>>
-{
-	using type = typename static_vector_add<static_vector<E1...>, static_vector<E2...>>::type;
-};
-
-template <int E1H, int... E1T, int E2H, int... E2T>
-struct static_vector_add<static_vector<E1H, E1T...>, static_vector<E2H, E2T...>>
-{
-	using type = typename static_vector_cat<static_vector<E1H + E2H>, typename static_vector_add<static_vector<E1T...>, static_vector<E2T...>>::type>::type;
-};
-
-template <int... E2>
-struct static_vector_add<static_vector<0>, static_vector<E2...>>
-{
-	using type = static_vector<E2...>;
-};
-
-template <int... E1>
-struct static_vector_add<static_vector<E1...>, static_vector<0>>
-{
-	using type = static_vector<E1...>;
-};
-
-template <>
-struct static_vector_add<static_vector<0>, static_vector<0>>
-{
-	using type = static_vector<0>;
-};
 
 enum class si_units
 {
